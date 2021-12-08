43823fdd8c40269800347ef03fe4c00ef4af7d54
TheDukeofErl <TheDukeofErl@users.noreply.github.com>
2021-12-07 19:25:38 -0600

    Update to match style guide, add sublime to .gitignore.

--- main.py
+++ main.py
@@ -13,49 +13,49 @@ from ui_py.replay_path_detection import Ui_ReplayPathDetection
 from utilities import replay_dir_manager
 
 class Window(QMainWindow, Ui_MainWindow):
-	def __init__(self, parent=None):
-		super().__init__(parent)
-		self.setupUi(self)
-		self.listview_replays_model = QStandardItemModel()
-		self.listview_replays.setModel(self.listview_replays_model)
-		self.connectSignalSlots()
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.listview_replays_model = QStandardItemModel()
+        self.listview_replays.setModel(self.listview_replays_model)
+        self.connectSignalSlots()
 
-	def connectSignalSlots(self):
-		self.actionOpen.triggered.connect(self.open)
-		self.actionExit.triggered.connect(self.close)
-		self.actionAbout.triggered.connect(self.about)
-		self.listview_replays.selectionModel().currentChanged.connect(self.replay_selected)
+    def connectSignalSlots(self):
+        self.actionOpen.triggered.connect(self.open)
+        self.actionExit.triggered.connect(self.close)
+        self.actionAbout.triggered.connect(self.about)
+        self.listview_replays.selectionModel().currentChanged.connect(self.replay_selected)
 
-	def open(self):
-		new_path = QFileDialog.getExistingDirectory()
-		if dir_manager.set_path(new_path):
-			self.reloadMenu(dir_manager.list_replays())
+    def open(self):
+        new_path = QFileDialog.getExistingDirectory()
+        if dir_manager.set_path(new_path):
+            self.reloadMenu(dir_manager.list_replays())
 
-	def about(self):
-		about = About(self)
-		about.exec()
+    def about(self):
+        about = About(self)
+        about.exec()
 
-	def replay_selected(self):
-		index = self.listview_replays.selectionModel().currentIndex()
-		selection = str(index.data())
-		self.statusbar.showMessage(dir_manager.replay_path_str + "/" + selection)
+    def replay_selected(self):
+        index = self.listview_replays.selectionModel().currentIndex()
+        selection = str(index.data())
+        self.statusbar.showMessage(dir_manager.replay_path_str + "/" + selection)
 
-	def reloadMenu(self, replays):
-		self.listview_replays_model.clear()
-		if replays != None:
-			for i in replays:
-				item = QStandardItem(i)
-				self.listview_replays_model.appendRow(item)
+    def reloadMenu(self, replays):
+        self.listview_replays_model.clear()
+        if replays != None:
+            for i in replays:
+                item = QStandardItem(i)
+                self.listview_replays_model.appendRow(item)
 
 class ReplayPathDetection(QDialog, Ui_ReplayPathDetection):
-	def __init__(self, parent=None):
-		super().__init__(parent)
-		self.setupUi(self)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
 
 class About(QDialog, Ui_About):
-	def __init__(self, parent=None):
-		super().__init__(parent)
-		self.setupUi(self)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
 
 # Setup main window
 app = QApplication(sys.argv)
--- utilities.py
+++ utilities.py
@@ -4,49 +4,49 @@ import platform
 from pathlib import Path
 
 class replay_dir_manager(object):
-	def __init__(self):
-		self.path_valid = False
-		self.replay_path = None
-		self.replay_path_str = "NO_PATH"
-		
-	def path_detection(self):
-		os_platform = platform.system()
-		print("Detected platform as: " + os_platform)
-		if os_platform == "Linux":
-			self.replay_path = Path.home().joinpath('.local','share','Skullgirls','Replays')
-		else:
-			return self.path_valid
+    def __init__(self):
+        self.path_valid = False
+        self.replay_path = None
+        self.replay_path_str = "NO_PATH"
+        
+    def path_detection(self):
+        os_platform = platform.system()
+        print("Detected platform as: " + os_platform)
+        if os_platform == "Linux":
+            self.replay_path = Path.home().joinpath('.local','share','Skullgirls','Replays')
+        else:
+            return self.path_valid
 
-		dir_list = os.listdir(self.replay_path)
-		if len(dir_list) == 1:
-			self.replay_path = self.replay_path.joinpath(dir_list[0])
-			self.replay_path_str = str(self.replay_path)
-			self.path_valid = True
-			print("Path detected as " + self.replay_path_str)
-		else:
-			self.path_valid = False
-			self.replay_path = None
-			self.replay_path_str = "NO_PATH"
+        dir_list = os.listdir(self.replay_path)
+        if len(dir_list) == 1:
+            self.replay_path = self.replay_path.joinpath(dir_list[0])
+            self.replay_path_str = str(self.replay_path)
+            self.path_valid = True
+            print("Path detected as " + self.replay_path_str)
+        else:
+            self.path_valid = False
+            self.replay_path = None
+            self.replay_path_str = "NO_PATH"
 
-		return self.path_valid
+        return self.path_valid
 
-	def set_path(self, new_path):
-		self.replay_path = Path(new_path)
-		print("New path: " + new_path)
-		if self.replay_path.exists() and self.replay_path.is_dir():
-			self.replay_path_str = str(self.replay_path)
-			self.path_valid = True
-		else:
-			self.path_valid = False
-			self.replay_path = None
-			self.replay_path_str = "NO_PATH"
+    def set_path(self, new_path):
+        self.replay_path = Path(new_path)
+        print("New path: " + new_path)
+        if self.replay_path.exists() and self.replay_path.is_dir():
+            self.replay_path_str = str(self.replay_path)
+            self.path_valid = True
+        else:
+            self.path_valid = False
+            self.replay_path = None
+            self.replay_path_str = "NO_PATH"
 
-		return self.path_valid
+        return self.path_valid
 
-	def list_replays(self):
-		print("Getting replays from : " + self.replay_path_str)
-		if self.path_valid:
-			replays = os.listdir(self.replay_path)
-			replays = [os.path.splitext(each)[0] for each in replays]
-			replays = list(dict.fromkeys(replays))
-			return replays
+    def list_replays(self):
+        print("Getting replays from : " + self.replay_path_str)
+        if self.path_valid:
+            replays = os.listdir(self.replay_path)
+            replays = [os.path.splitext(each)[0] for each in replays]
+            replays = list(dict.fromkeys(replays))
+            return replays
